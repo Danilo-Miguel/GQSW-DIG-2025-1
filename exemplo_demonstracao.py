3e557ea8abe55aebc96653896cc33bf500223dae
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2025-04-09 15:51:52 -0300

    Feat: Demostration code without adjust

---
+++ exemplo_demonstracao.py
@@ -0,0 +1,29 @@
+"""Este módulo contém funções para demonstração de boas práticas de código."""
+
+def calcula(valor1, valor2, operacao):
+    if operacao == "soma":
+        resultado = valor1 + valor2
+        print("O resultado é:", resultado)
+    elif operacao == "subtrai":
+        resultado = valor1 - valor2
+        print("O resultado é:", resultado)
+    elif operacao == "multiplica":
+        resultado = valor1 * valor2
+        print("O resultado é:", resultado)
+    elif operacao == "divide":
+        if valor2 != 0:
+            resultado = valor1 / valor2
+            print("O resultado é:", resultado)
+        else:
+            print("Não é possível dividir por zero")
+    else:
+        print("Operação inválida")
+
+def main():
+    calcula(10, 2, "soma")
+    calcula(10, 2, "subtrai")
+    calcula(10, 2, "multiplica")
+    calcula(10, 0, "divide")
+    calcula(10, 2, "exponencia")
+
+main()
